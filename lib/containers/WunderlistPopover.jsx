62265a09713890101ce38ffed1827aa8d47931be
Miguel Rosales <miguel.rosales@jadu.co.uk>
2017-06-02 18:18:54 +0100

    Cleaned up and added docblocks to containers

--- lib/containers/WunderlistPopover.jsx
+++ lib/containers/WunderlistPopover.jsx
@@ -1,70 +1,44 @@
-import {Actions, React} from "nylas-exports";
-import {FixedPopover, Menu, Spinner} from "nylas-component-kit";
-import WunderlistStore from "../stores/WunderlistStore";
-import Icon from "../components/Icon";
-import List from "../model/List";
-import ToDo from "../model/ToDo";
+import { FixedPopover, Menu, Spinner } from 'nylas-component-kit'
+import { Actions, React } from 'nylas-exports'
+import Icon from '../components/Icon'
+import List from '../model/List'
+import Task from '../model/Task'
+import WunderlistStore from '../stores/WunderlistStore'
 
-export default class WunderlistPopover extends FixedPopover {
-
-    static displayName = 'WunderlistPopover';
+/**
+ * Component to manage the Wunderlist popover.
+ *
+ * @author Miguel Rosales Sueiro
+ */
+export default class WunderlistPopover extends FixedPopover
+{
+    static displayName = 'WunderlistPopover'
 
     static propTypes = {
         thread: React.PropTypes.object.isRequired,
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = this.getStateFromStores();
     }
 
-    componentDidMount() {
-        this.unsubscribe = WunderlistStore.listen(this.onChange);
+    constructor(props)
+    {
+        super(props)
+        this.state = WunderlistPopover.getStateFromStores()
     }
 
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    onChange = () => {
-        this.setState(this.getStateFromStores());
-    };
-
-    getStateFromStores = () => {
+    static getStateFromStores()
+    {
         return {
             account: WunderlistStore.getAccount(),
-        };
-    };
-
-    onSelectList(list) {
-        const {thread} = this.props;
-
-        if (!list instanceof List) {
-            return;
         }
-
-        WunderlistStore.postToDo(
-            new ToDo({
-                list_id: list.getId(),
-                title: thread.subject,
-            })
-        );
-
-        Actions.closePopover();
     }
 
-    onEscape = () => {
-        Actions.closePopover();
-    };
+    static onEscape()
+    {
+        Actions.closePopover()
+    }
 
-    buildListPickerMenuItems() {
-        const {account} = this.state;
-
-        return account.getListsSorted().toArray();
-    };
-
-    renderMenuItem = (item) => {
-        const iconLeft = item.isInbox() ? 'inbox' : 'list';
+    static renderMenuItem(item)
+    {
+        const iconLeft = item.isInbox() ? 'inbox' : 'list'
 
         return (
             <div key={item.getId()}>
@@ -72,27 +46,68 @@ export default class WunderlistPopover extends FixedPopover {
                 {item.getTitle()}
                 <span style={styles.iconRight}><Icon icon='chevron-right'/></span>
             </div>
-        );
+        )
+    }
+
+    buildListPickerMenuItems()
+    {
+        const {account} = this.state
+
+        return account.getListsSorted().toArray()
     };
 
-    render() {
+    componentDidMount()
+    {
+        this.unsubscribe = WunderlistStore.listen(this.onChange)
+    }
+
+    componentWillUnmount()
+    {
+        this.unsubscribe()
+    }
+
+    onChange()
+    {
+        this.setState(WunderlistPopover.getStateFromStores())
+    }
+
+    onSelectList(list)
+    {
+        const {thread} = this.props
+
+        if (!list instanceof List) {
+            return
+        }
+
+        WunderlistStore.postTask(
+            new Task({
+                list_id: list.getId(),
+                title: thread.subject,
+            }),
+        )
+
+        Actions.closePopover()
+    }
+
+    render()
+    {
         const headerComponents = [
             <p style={styles.menuHeader}>Select a list:</p>,
-        ];
+        ]
 
         return (
             <div style={styles.popover}>
                 <Menu
-                    headerComponents={headerComponents}
                     footerComponents={[]}
-                    itemContent={this.renderMenuItem.bind(this)}
+                    headerComponents={headerComponents}
+                    itemContent={WunderlistPopover.renderMenuItem.bind(this)}
                     itemKey={item => item.get('id').toString()}
                     items={this.buildListPickerMenuItems()}
+                    onEscape={WunderlistPopover.onEscape}
                     onSelect={this.onSelectList.bind(this)}
-                    onEscape={this.onEscape}
                 />
             </div>
-        );
+        )
     }
 }
 
@@ -113,4 +128,4 @@ const styles = {
         overflow: 'auto',
         width: 250,
     },
-};
+}
--- lib/containers/WunderlistToolbarButton.jsx
+++ lib/containers/WunderlistToolbarButton.jsx
@@ -1,44 +1,46 @@
-import {React, ReactDOM, Actions} from 'nylas-exports';
-import {RetinaImg, Menu, MenuItem, Toast} from 'nylas-component-kit';
-import WunderlistPopover from './WunderlistPopover';
+import { Menu, MenuItem, RetinaImg, Toast } from 'nylas-component-kit'
+import { Actions, React, ReactDOM } from 'nylas-exports'
+import WunderlistPopover from './WunderlistPopover'
 
 /**
  * Toolbar button to add an email as a Wunderlist Task.
  */
-export default class WunderlistToolbarButton extends React.Component {
-
-    static displayName = 'WunderlistToolbarButton';
+export default class WunderlistToolbarButton extends React.Component
+{
+    static displayName = 'WunderlistToolbarButton'
 
     static propTypes = {
         // This comes in the props because it is registered as a thread action button.
         thread: React.PropTypes.object.isRequired,
-    };
+    }
 
-    onClick() {
-        const buttonRectangle = ReactDOM.findDOMNode(this.refs.wunderlist_button).getBoundingClientRect();
+    onClick()
+    {
+        const buttonRectangle = ReactDOM.findDOMNode(this.refs.wunderlist_button).getBoundingClientRect()
 
         Actions.openPopover(
             <WunderlistPopover thread={this.props.thread}/>,
             {
                 originRect: buttonRectangle,
                 direction: 'down',
-            }
-        );
+            },
+        )
     }
 
-    render() {
+    render()
+    {
         return (
             <button
                 className={'btn btn-toolbar'}
                 onClick={this.onClick.bind(this)}
-                title='Add to Wunderlist'
                 ref='wunderlist_button'
+                title='Add to Wunderlist'
             >
                 <RetinaImg
                     mode={RetinaImg.Mode.ContentIsMask}
                     url='nylas://nylas-wunderlist/assets/nylas-wunderlist-toolbar@2x.png'
                 />
             </button>
-        );
+        )
     }
 }
